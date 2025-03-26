308e71563dd162d6d61636ce7049a1d500700ee7
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-03-26 17:11:09 +0700

    ...

--- gomint-server/src/main/java/io/gomint/server/network/Protocol.java
+++ gomint-server/src/main/java/io/gomint/server/network/Protocol.java
@@ -59,8 +59,8 @@ public final class Protocol {
     // MC:PE Protocol ID
     public static final int MINECRAFT_PE_BETA_PROTOCOL_VERSION = -1;
     public static final int MINECRAFT_PE_NEXT_STABLE_PROTOCOL_VERSION = -1;
-    public static final int MINECRAFT_PE_PROTOCOL_VERSION = 422;
-    public static final String MINECRAFT_PE_NETWORK_VERSION = "1.16.200";
+    public static final int MINECRAFT_PE_PROTOCOL_VERSION = 786;
+    public static final String MINECRAFT_PE_NETWORK_VERSION = "1.21.70";
 
     // ========================================= PACKET IDS ========================================= //
     public static final byte BATCH_MAGIC = (byte) 0xfe;
